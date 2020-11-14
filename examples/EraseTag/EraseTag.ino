7631aa5b6dd8768ed783aba8af00f27569d966a7
Nitek <nitek@blickt.es>
2020-11-14 14:06:00 +0100

    Ported examples

--- examples/EraseTag/EraseTag.ino
+++ examples/EraseTag/EraseTag.ino
@@ -1,23 +1,14 @@
 // Erases a NFC tag by writing an empty NDEF message 
 
-#if 0
 #include <SPI.h>
-#include <PN532_SPI.h>
-#include <PN532.h>
-#include <NfcAdapter.h>
+#include <MFRC522.h>
+#include "NfcAdapter.h"
 
-PN532_SPI pn532spi(SPI, 10);
-NfcAdapter nfc = NfcAdapter(pn532spi);
-#else
+#define SS_PIN D8
 
-#include <Wire.h>
-#include <PN532_I2C.h>
-#include <PN532.h>
-#include <NfcAdapter.h>
+MFRC522 mfrc522(SS_PIN, UINT8_MAX); // Create MFRC522 instance
 
-PN532_I2C pn532_i2c(Wire);
-NfcAdapter nfc = NfcAdapter(pn532_i2c);
-#endif
+NfcAdapter nfc = NfcAdapter(&mfrc522);
 
 void setup(void) {
     Serial.begin(9600);
