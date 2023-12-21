92ac6b4cd44a92fe73a03d51882fa031049777da
Michael Haberler <git@mah.priv.at>
2023-12-21 18:20:19 +0100

    start on toJson() method
    convert NDEF message to ArduinoJson

--- src/NdefMessage.h
+++ src/NdefMessage.h
@@ -3,40 +3,44 @@
 
 #include <Ndef.h>
 #include <NdefRecord.h>
+#include <ArduinoJson.h>
+#include <HexDump.h>
 
 #define MAX_NDEF_RECORDS 4
 
-class NdefMessage
-{
+class NdefMessage {
 public:
-    NdefMessage(void);
-    NdefMessage(const byte *data, const uint16_t numBytes);
-    NdefMessage(const NdefMessage &rhs);
-    ~NdefMessage();
-    NdefMessage &operator=(const NdefMessage &rhs);
+  NdefMessage(void);
+  NdefMessage(const byte *data, const uint16_t numBytes);
+  NdefMessage(const NdefMessage &rhs);
+  ~NdefMessage();
+  NdefMessage &operator=(const NdefMessage &rhs);
 
-    unsigned int getEncodedSize(); // need so we can pass array to encode
-    void encode(byte *data);
+  unsigned int getEncodedSize();  // need so we can pass array to encode
+  void encode(byte *data);
 
-    boolean addRecord(NdefRecord &record);
-    void addMimeMediaRecord(const char *mimeType, const char *payload);
-    void addMimeMediaRecord(const char *mimeType, byte *payload, const uint16_t payloadLength);
-    void addTextRecord(const char *text);
-    void addTextRecord(const char *text, const char *encoding);
-    void addUriRecord(const char *uri);
-    void addExternalRecord(const char *type, const byte *payload, const uint16_t payloadLength);
-    void addEmptyRecord();
+  boolean addRecord(NdefRecord &record);
+  void addMimeMediaRecord(const char *mimeType, const char *payload);
+  void addMimeMediaRecord(const char *mimeType, byte *payload,
+                          const uint16_t payloadLength);
+  void addTextRecord(const char *text);
+  void addTextRecord(const char *text, const char *encoding);
+  void addUriRecord(const char *uri);
+  void addExternalRecord(const char *type, const byte *payload,
+                         const uint16_t payloadLength);
+  void addEmptyRecord();
+  bool toJson(JsonDocument &result);
 
-    uint8_t getRecordCount();
-    NdefRecord getRecord(uint8_t index);
-    NdefRecord operator[](uint8_t index);
+  uint8_t getRecordCount();
+  NdefRecord getRecord(uint8_t index);
+  NdefRecord operator[](uint8_t index);
 
 #ifdef NDEF_USE_SERIAL
-    void print();
+  void print();
 #endif
 private:
-    NdefRecord *_records[MAX_NDEF_RECORDS];
-    uint8_t _recordCount;
+  NdefRecord *_records[MAX_NDEF_RECORDS];
+  uint8_t _recordCount;
 };
 
 #endif
