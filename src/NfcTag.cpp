050f9350c816d755a4aa5b42e38c6ae7c129c27a
Michael Haberler <git@mah.priv.at>
2024-01-03 19:30:55 +0100

    split toJson into distinct methods for cardinfo and NDEF message

--- src/NfcTag.cpp
+++ src/NfcTag.cpp
@@ -1,47 +1,40 @@
 #include <NfcTag.h>
 #include <MFRC522Debug.h>
 
-NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType)
-{
+NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType) {
     _taginfo = tag;
     _tagType = tagType;
     _ndefMessage = (NdefMessage *)NULL;
     _isFormatted = false;
 }
 
-NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType, bool isFormatted)
-{
+NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType, bool isFormatted) {
     _taginfo = tag;
     _tagType = tagType;
     _ndefMessage = (NdefMessage *)NULL;
     _isFormatted = isFormatted;
 }
 
-NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType, NdefMessage &ndefMessage)
-{
+NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType, NdefMessage &ndefMessage) {
     _taginfo = tag;
     _tagType = tagType;
     _ndefMessage = new NdefMessage(ndefMessage);
     _isFormatted = true; // If it has a message it's formatted
 }
 
-NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType, const byte *ndefData, const uint16_t ndefDataLength)
-{
+NfcTag::NfcTag(const TagInfo& tag, PICC_Type tagType, const byte *ndefData, const uint16_t ndefDataLength) {
     _taginfo = tag;
     _tagType = tagType;
     _ndefMessage = new NdefMessage(ndefData, ndefDataLength);
     _isFormatted = true; // If it has a message it's formatted
 }
 
-NfcTag::~NfcTag()
-{
+NfcTag::~NfcTag() {
     delete _ndefMessage;
 }
 
-NfcTag &NfcTag::operator=(const NfcTag &rhs)
-{
-    if (this != &rhs)
-    {
+NfcTag &NfcTag::operator=(const NfcTag &rhs) {
+    if (this != &rhs) {
         delete _ndefMessage;
         _taginfo = rhs._taginfo;
         _ndefMessage = new NdefMessage(*rhs._ndefMessage);
@@ -49,19 +42,16 @@ NfcTag &NfcTag::operator=(const NfcTag &rhs)
     return *this;
 }
 
-uint8_t NfcTag::getUidLength()
-{
+uint8_t NfcTag::getUidLength() {
     return _taginfo.uid.size;
 }
 
-void NfcTag::getUid(byte *uid, uint8_t *uidLength)
-{
+void NfcTag::getUid(byte *uid, uint8_t *uidLength) {
     memcpy(uid, _taginfo.uid.uidByte, _taginfo.uid.size < *uidLength ? _taginfo.uid.size : *uidLength);
     *uidLength = _taginfo.uid.size;
 }
 
-String NfcTag::getUidString()
-{
+String NfcTag::getUidString() {
     if (_taginfo.uid.size == 0)
         return "";
     char *hd =  buildHexData (NULL, (uint8_t*)_taginfo.uid.uidByte, _taginfo.uid.size);
@@ -70,14 +60,12 @@ String NfcTag::getUidString()
     return uidString;
 }
 
-size_t NfcTag::getAtsSize()
-{
+size_t NfcTag::getAtsSize() {
     return _taginfo.ats.size;
 }
 
 String
-NfcTag::getAtsString ()
-{
+NfcTag::getAtsString () {
     if (_taginfo.ats.size == 0)
         return "";
     char *hd
@@ -87,64 +75,60 @@ NfcTag::getAtsString ()
     return atsString;
 }
 
-MFRC522Constants::PICC_Type NfcTag::getTagType()
-{
+MFRC522Constants::PICC_Type NfcTag::getTagType() {
     return _tagType;
 }
 
-bool NfcTag::hasNdefMessage()
-{
+bool NfcTag::hasNdefMessage() {
     return (_ndefMessage != NULL);
 }
 
-NdefMessage NfcTag::getNdefMessage()
-{
+NdefMessage NfcTag::getNdefMessage() {
     return *_ndefMessage;
 }
 
-bool NfcTag::isFormatted()
-{
+bool NfcTag::isFormatted() {
     return _isFormatted;
 }
 
 #if NDEF_USE_SERIAL
 
-void NfcTag::print()
-{
+void NfcTag::print() {
     Serial.print(F("NFC Tag - "));
     Serial.println(_tagType);
     Serial.print(F("UID "));
     Serial.println(getUidString());
-    if (_ndefMessage == NULL)
-    {
+    if (_ndefMessage == NULL) {
         Serial.println(F("\nNo NDEF Message"));
-    }
-    else
-    {
+    } else {
         _ndefMessage->print();
     }
 }
 #endif
 
 bool
-NfcTag::toJson (JsonDocument &doc)
-{
-    if (getUidLength ())
-        {
-            doc["uid"] = getUidString ();
-            doc["sak"] = String (_taginfo.uid.sak, HEX);
-            doc["atqa"] = String (_taginfo.atqa, HEX);
-            doc["type"] = _tagType;
-            doc["picc"] = MFRC522Debug::PICC_GetTypeName (_tagType);
-            if (getAtsSize ())
-                {
-                    doc["ats"] = getAtsString ();
-                }
+NfcTag::tagToJson (JsonDocument &doc) {
+    if (getUidLength ()) {
+        doc["uid"] = getUidString ();
+        doc["sak"] = String (_taginfo.uid.sak, HEX);
+        doc["atqa"] = String (_taginfo.atqa, HEX);
+        doc["type"] = _tagType;
+        doc["picc"] = MFRC522Debug::PICC_GetTypeName (_tagType);
+        if (getAtsSize ()) {
+            doc["ats"] = getAtsString ();
         }
-    if (hasNdefMessage ())
-        {
-            JsonArray ndef = doc.createNestedArray ("ndef");
-            getNdefMessage ().toJson (ndef);
-        }
-    return true;
+        return true;
+    }
+    return false;
+}
+
+bool
+NfcTag::tagToJson (JsonDocument &doc) {
+
+    if (hasNdefMessage ()) {
+        JsonArray ndef = doc.createNestedArray ("ndef");
+        getNdefMessage ().toJson (ndef);
+        return true;
+    }
+    return false;
 }
--- src/NfcTag.h
+++ src/NfcTag.h
@@ -9,47 +9,49 @@
 #include <MFRC522Constants.h>
 #include <MFRC522Extended.h>
 
-class NfcTag
-{
+class NfcTag {
 
-public:
-  using Uid = MFRC522Constants::Uid;
-  using PICC_Type = MFRC522Constants::PICC_Type;
-  using StatusCode = MFRC522Constants::StatusCode;
-  using TagInfo = MFRC522Extended::TagInfo;
+  public:
+    using Uid = MFRC522Constants::Uid;
+    using PICC_Type = MFRC522Constants::PICC_Type;
+    using StatusCode = MFRC522Constants::StatusCode;
+    using TagInfo = MFRC522Extended::TagInfo;
 
-  NfcTag(const TagInfo& tag, PICC_Type tagType);
-  NfcTag(const TagInfo& tag, PICC_Type tagType, bool isFormatted);
-  NfcTag(const TagInfo& tag, PICC_Type tagType, NdefMessage &ndefMessage);
-  NfcTag(const TagInfo& tag, PICC_Type tagType, const byte *ndefData, const uint16_t ndefDataLength);
-  ~NfcTag(void);
-  NfcTag &operator=(const NfcTag &rhs);
-  uint8_t getUidLength();
-  void getUid(byte *uid, uint8_t *uidLength);
-  String getUidString();
-  size_t getAtsSize();
-  String getAtsString();
-  PICC_Type getTagType();
-  bool hasNdefMessage();
-  NdefMessage getNdefMessage();
-  bool isFormatted();
-  bool toJson(JsonDocument &result);
-  StatusCode getStatus() { return _status; };
+    NfcTag(const TagInfo& tag, PICC_Type tagType);
+    NfcTag(const TagInfo& tag, PICC_Type tagType, bool isFormatted);
+    NfcTag(const TagInfo& tag, PICC_Type tagType, NdefMessage &ndefMessage);
+    NfcTag(const TagInfo& tag, PICC_Type tagType, const byte *ndefData, const uint16_t ndefDataLength);
+    ~NfcTag(void);
+    NfcTag &operator=(const NfcTag &rhs);
+    uint8_t getUidLength();
+    void getUid(byte *uid, uint8_t *uidLength);
+    String getUidString();
+    size_t getAtsSize();
+    String getAtsString();
+    PICC_Type getTagType();
+    bool hasNdefMessage();
+    NdefMessage getNdefMessage();
+    bool isFormatted();
+    bool tagToJson(JsonDocument &result);
+    bool ndefToJson(JsonDocument &result);
+    StatusCode getStatus() {
+        return _status;
+    };
 
 #if NDEF_USE_SERIAL
-  void print();
+    void print();
 #endif
-private:
-  TagInfo _taginfo;
-  PICC_Type _tagType;
-  StatusCode _status;
-  NdefMessage *_ndefMessage;
-  /**
-   * if tag is not formatted it is most probably in HALTED state as soon as we realize that
-   * because authentication failed => We need to call PICC_WakeupA
-   */
-  bool _isFormatted;
-  // TODO capacity
+  private:
+    TagInfo _taginfo;
+    PICC_Type _tagType;
+    StatusCode _status;
+    NdefMessage *_ndefMessage;
+    /**
+     * if tag is not formatted it is most probably in HALTED state as soon as we realize that
+     * because authentication failed => We need to call PICC_WakeupA
+     */
+    bool _isFormatted;
+    // TODO capacity
 };
 
 #endif
